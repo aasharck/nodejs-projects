d7ac45d85333d10f6082d85446d9064ce7f14bb1
Aashar ck <aasharck@gmail.com>
2023-08-01 09:33:25 +0300

    testing

--- node-crash-course/3/server.js
+++ node-crash-course/3/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('request made to the server');
+  console.log('request made');
 });
 
 server.listen(3000, 'localhost', () => {
